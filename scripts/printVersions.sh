281bea699138f32154012762a2193ea0c14216a9
DrSnowbird <DrSnowbird@openkbs.org>
2021-12-15 22:52:02 -0500

    updated ./jdk-mvn-py3/scripts/printVersions.sh

--- scripts/printVersions.sh
+++ scripts/printVersions.sh
@@ -1,17 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "JAVA_HOME=$JAVA_HOME"
 whereis java
 echo
-java -version
-mvn --version
-python -V
-python3 -V
-pip --version
-pip3 --version
-gradle --version
-npm -v
-node -v
-yarn -V
+which java && java -version
+which mvn && mvn --version
+which python && python -V
+which python3 && python3 -V
+which pip && pip --version
+which pip3 && pip3 --version
+which gradle && gradle --version
+which npm && npm -v
+which node && node -v
 cat /etc/*-release
-
+which ant && ant -version 
